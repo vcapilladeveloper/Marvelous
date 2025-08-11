ce14430f91f3a7eeee2f3e1be2b8215787b7591b
Victor Capilla <victorcapilladeveloper@gmail.com>
2025-08-11 18:58:50 +0200

    Modify Shimmer

--- Marvelous-iOS/DesignSystem/Sources/DesignSystem/Components/AsyncRemoteImage.swift
+++ Marvelous-iOS/DesignSystem/Sources/DesignSystem/Components/AsyncRemoteImage.swift
@@ -44,8 +44,6 @@ public struct AsyncRemoteImage: View {
         }
         // Dark shadow bottom-right
         .shadow(color: DSPalette.shadow.opacity(0.4), radius: 4, x: 4, y: 4)
-        // Light shadow top-left
-        .shadow(color: .white.opacity(0.7), radius: 4, x: -4, y: -4)
         .accessibilityHidden(true)
     }
 }
